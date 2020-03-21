a5bf761cc3fd7c39cb6f4812f9969895e9f59813
Michael Garcia <thecrazygm@gmail.com>
2020-03-21 13:36:37 -0500

    added steem/sbd back

--- bhivegraphenebase/chains.py
+++ bhivegraphenebase/chains.py
@@ -9,6 +9,8 @@ known_chains = {
         "min_version": '0.23.0',
         "prefix": "STM",
         "chain_assets": [
+            {"asset": "@@000000013", "symbol": "SBD", "precision": 3, "id": 0},
+            {"asset": "@@000000021", "symbol": "STEEM", "precision": 3, "id": 1},
             {"asset": "@@000000013", "symbol": "HBD", "precision": 3, "id": 0},
             {"asset": "@@000000021", "symbol": "HIVE", "precision": 3, "id": 1},
             {"asset": "@@000000037", "symbol": "VESTS", "precision": 6, "id": 2}
